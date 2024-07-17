1f26bbd1a6b3f38ac2c969aab01415048b1bccb1
Naury <koertyf@gmail.com>
2024-07-17 13:08:34 +0400

    Add delivery in order

--- python_ym/api_objects/order.py
+++ python_ym/api_objects/order.py
@@ -4,6 +4,19 @@ import datetime
 
 class Order(ApiObject):
 
+    class Delivery(ApiObject):
+
+        class Dates(ApiObject):
+            realDeliveryDate: str = None
+
+            def __init__(self, data):
+                super().__init__(data)
+
+        dates: Dates = None
+
+        def __init__(self, data):
+            super().__init__(data)
+
     class Subsidie(ApiObject):
 
         type: str = None
@@ -34,6 +47,7 @@ class Order(ApiObject):
     deliveryTotal = None
     items: list[ItemOfOrder] = None
     subsidies: list[Subsidie] = None
+    delivery: Delivery = None
 
     # Not Receivable via API
     total: float = None
@@ -45,6 +59,9 @@ class Order(ApiObject):
 
         self.creationDate = datetime.datetime.strptime(self.creationDate, '%d-%m-%Y %H:%M:%S')
         self.creationDate = self.creationDate - datetime.timedelta(hours=3)
+
+        if self.delivery.dates.realDeliveryDate:
+            self.delivery.dates.realDeliveryDate = datetime.datetime.strptime(self.delivery.dates.realDeliveryDate, '%d-%m-%Y')
 
         self.subsidyTotal = sum(subsidy.amount for subsidy in self.subsidies) if self.subsidies else 0
         self.total = self.itemsTotal + self.deliveryTotal
